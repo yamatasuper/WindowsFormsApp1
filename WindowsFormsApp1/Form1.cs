5790041c2848a4534dd6e0d60cf020224e60aefa
Артем Мовчан <mob.ar@mail.ru>
2024-04-04 19:21:54 +0400

    Windows Programming Student

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> words = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            words.Add("а", "a");
+            words.Add("б", "b");
+            words.Add("в", "v");
+            words.Add("г", "g");
+            words.Add("д", "d");
+            words.Add("е", "e");
+            words.Add("ё", "yo");
+            words.Add("ж", "zh");
+            words.Add("з", "z");
+            words.Add("и", "i");
+            words.Add("й", "j");
+            words.Add("к", "k");
+            words.Add("л", "l");
+            words.Add("м", "m");
+            words.Add("н", "n");
+            words.Add("о", "o");
+            words.Add("п", "p");
+            words.Add("р", "r");
+            words.Add("с", "s");
+            words.Add("т", "t");
+            words.Add("у", "u");
+            words.Add("ф", "f");
+            words.Add("х", "h");
+            words.Add("ц", "c");
+            words.Add("ч", "ch");
+            words.Add("ш", "sh");
+            words.Add("щ", "sch");
+            words.Add("ъ", "j");
+            words.Add("ы", "i");
+            words.Add("ь", "j");
+            words.Add("э", "e");
+            words.Add("ю", "yu");
+            words.Add("я", "ya");
+            words.Add("А", "A");
+            words.Add("Б", "B");
+            words.Add("В", "V");
+            words.Add("Г", "G");
+            words.Add("Д", "D");
+            words.Add("Е", "E");
+            words.Add("Ё", "Yo");
+            words.Add("Ж", "Zh");
+            words.Add("З", "Z");
+            words.Add("И", "I");
+            words.Add("Й", "J");
+            words.Add("К", "K");
+            words.Add("Л", "L");
+            words.Add("М", "M");
+            words.Add("Н", "N");
+            words.Add("О", "O");
+            words.Add("П", "P");
+            words.Add("Р", "R");
+            words.Add("С", "S");
+            words.Add("Т", "T");
+            words.Add("У", "U");
+            words.Add("Ф", "F");
+            words.Add("Х", "H");
+            words.Add("Ц", "C");
+            words.Add("Ч", "Ch");
+            words.Add("Ш", "Sh");
+            words.Add("Щ", "Sch");
+            words.Add("Ъ", "J");
+            words.Add("Ы", "I");
+            words.Add("Ь", "J");
+            words.Add("Э", "E");
+            words.Add("Ю", "Yu");
+            words.Add("Я", "Ya");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string source = textBox1.Text;
+            foreach (KeyValuePair<string, string> pair in words)
+            {
+                source = source.Replace(pair.Key, pair.Value);
+            }
+            textBox2.Text = source;
+
+        }
+     
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string lines = "full address:s:" + textBox1.Text;
+            System.IO.File.WriteAllText(@"C:\Users\Артемий\Documents\Visual Studio 2017\Projects\WindowsFormsApp1\time.txt", lines);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile1 = new SaveFileDialog();
+            saveFile1.DefaultExt = "*.txt";
+            saveFile1.Filter = "Text files|*.txt";
+            if (saveFile1.ShowDialog() == System.Windows.Forms.DialogResult.OK &&
+                saveFile1.FileName.Length > 0)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFile1.FileName, true))
+                {
+                    sw.WriteLine(textBox1.Text + " = " + textBox2.Text);
+                    sw.Close();
+                }
+            }
+        }
+    }
+}
